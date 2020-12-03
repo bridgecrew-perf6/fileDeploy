69cebbc814bac20f5454f427647da0a4d6a34c8f
daniyalAhmed-code <70039650+daniyalAhmed-code@users.noreply.github.com>
2020-12-04 00:36:16 +0500

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.{BuildNumber}.jar'
 cd /usr/bin
-ls -la
 sudo systemctl restart httpd
