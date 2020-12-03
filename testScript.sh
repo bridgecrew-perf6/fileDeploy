c19323a92098fef7a3536bab1e80d9e86c6f992e
Nouman Iftikhar <noumansatti7@gmail.com>
2020-12-04 00:42:05 +0500

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.{BuildNumber}.jar'
+curl  -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.{BuildNumber}.jar'
 cd /usr/bin
+ls -la
 sudo systemctl restart httpd
