80d2c06986de2dced1e303cc83cf92dc98530445
Nouman Iftikhar <noumansatti7@gmail.com>
2020-12-04 00:20:47 +0500

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-eurus/spring-boot-hello-world-1.0.0-SNAPSHOT.{BuildNumber}.jar'
-cd /usr/bin
-ls -la
-sudo systemctl restart httpd
+echo "admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-eurus/spring-boot-hello-world-1.0.0-SNAPSHOT.{BuildNumber}.jar'"
+#cd /usr/bin
+#ls -la
+#sudo systemctl restart httpd
