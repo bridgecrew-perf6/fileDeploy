99435c4beaa0088afba5f31714084e90b90799a5
Nouman72884 <noumansatti7@gmail.com>
2020-12-04 04:36:18 +0500

    initial commit

--- deploy-jenkinsfile/testScript.sh
+++ deploy-jenkinsfile/testScript.sh
@@ -2,11 +2,11 @@
 set +x
 APPLIACTION={APPLICATION_NAME}
 echo "$APPLIACTION"
-if [[ $APPLICATION == frontend ]]
+if [ $APPLICATION == 'frontend' ]
 then
   echo "frontend is downloading"
   curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.{BuildNumber}.jar'
-elif [[ $APPLICATION == backend ]]
+elif [ $APPLICATION == 'backend' ]
 then
   echo "backend is downloading"
   curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.11.jar'
