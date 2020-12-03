4fd5b704bb3e3326ad98ede5481b462e74f83b93
Nouman72884 <noumansatti7@gmail.com>
2020-12-04 04:09:47 +0500

    initial commit

--- deploy-jenkinsfile/testScript.sh
+++ deploy-jenkinsfile/testScript.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set +x
-echo -n "Enter a name: "
-if [[ {APPLICATION} -eq 'frontend' ]]
+
+
+if [[ $APPLICATION -eq 'frontend' ]]
 then
   curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.{BuildNumber}.jar'
-elif [[ {APPLICATION} -eq 'backend' ]]
+elif [[ $APPLICATION -eq 'backend' ]]
 then
   curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.11.jar'
 else
