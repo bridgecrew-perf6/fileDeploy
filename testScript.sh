3ccc081a7065885c0595b98e914fb52868cb3ce2
daniyalAhmed-code <70039650+daniyalAhmed-code@users.noreply.github.com>
2020-12-03 23:08:37 +0500

    Update testScript.sh

--- testScript.sh
+++ testScript.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-echo 'http://artifactory.eurustechnologies.info/artifactory/docker-eurus/spring-boot-hello-world-1.0.0-SNAPSHOT.foo.jar'
+curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-eurus/spring-boot-hello-world-1.0.0-SNAPSHOT.{BuildNumber}.jar'
+cd /usr/bin
+ls -la
+sudo systemctl restart httpd
