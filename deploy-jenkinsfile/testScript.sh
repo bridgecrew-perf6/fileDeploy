53e464ee19630037a96862d37671b86612aade63
Nouman72884 <noumansatti7@gmail.com>
2020-12-04 04:05:09 +0500

    initial commit

--- deploy-jenkinsfile/testScript.sh
+++ deploy-jenkinsfile/testScript.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set +x
-curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.{BuildNumber}.jar'
-#sudo cp my-app-1.0-SNAPSHOT.{BuildNumber}.jar /var/www/html
-#sudo chmod 777 /var/www/html
-#cd  /var/www/html
-#sudo rm index.html
-#nohup java -jar my-app-1.0-SNAPSHOT.{BuildNumber}.jar > index.html 2>&1 &
+echo -n "Enter a name: "
+if [[ {APPLICATION} -eq 'frontend' ]]
+then
+  curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.{BuildNumber}.jar'
+elif [[ {APPLICATION} -eq 'backend' ]]
+then
+  curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.11.jar'
+else
+  echo "no argument passed"
+fi
+
 sudo cp java-webapp.service /etc/systemd/system/
 sudo systemctl restart httpd
 sudo systemctl daemon-reload
