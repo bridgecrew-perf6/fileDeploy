0bc4ea8071868d45f93e4fed532bca9d97dae5fd
Nouman72884 <noumansatti7@gmail.com>
2020-12-04 05:10:11 +0500

    initial commit

--- deploy-jenkinsfile/testScript.sh
+++ deploy-jenkinsfile/testScript.sh
@@ -5,10 +5,10 @@ APPLICATION={APPLICATION_NAME}
 echo $APPLICATION
 if [ "$APPLICATION" = "frontend" ]; then
   echo "frontend is downloading"
-  #curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.{BuildNumber}.jar'
+  curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.{BuildNumber}.jar'
 elif [ "$APPLICATION" = "backend" ]; then
   echo "backend is downloading"
-  #curl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.11.jar'
+  scurl -sSf -u admin:{Token} -O 'http://artifactory.eurustechnologies.info/artifactory/docker-maven/my-app-1.0-SNAPSHOT.11.jar'
 else
   echo "no argument passed"
 fi
